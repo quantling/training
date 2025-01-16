a2abc89636ed63b3a3a2b537b20f74b63027c339
JoJoBarthold2 <valentin.j.schmidt@web.de>
2025-01-16 19:08:54 +0100

    made the word collection a little nicer

--- collect_words.py
+++ collect_words.py
@@ -1,43 +1,57 @@
 
-data_path = "../../../../mnt/Restricted/Corpora/CommonVoiceVTL/corpus_as_df_mp_folder_en"
+data_path = "../../../../mnt/Restricted/Corpora/CommonVoiceVTL/corpus_as_df_mp_folder_de"
 import logging
 import os
 logging.basicConfig(level=logging.INFO)
-
-
-sorted = sorted(os.listdir(data_path)) #sorting the files for better debugging
-for files in sorted:
-    logging.info(f"Processing {files}")
-import psutil
-
 from collections import Counter
 import os
 import pandas as pd
 import pickle
-words = Counter()
-i = 0
-word_list = []
-for files in sorted:
-        data = None
-        if i % 10 == 0:
-            pickle.dump(words,open(f"lexical_words_{i}.pkl","wb"))
-        if files.endswith(".pkl"):
-            data = pd.read_pickle(os.path.join(data_path,files))
-            if isinstance(data, pd.DataFrame):
-                print(data.columns)
-                for word in list(data["lexical_word"]):
-                    words[word] += 1
-                    word_list.append(word)
-            else:
-                print(f"Data is not a DataFrame: {files}")
-        []
-        i += 1
-        print(f"Processed {i} files")
-        print(files)
-        print(f"Memory usage: {psutil.virtual_memory().percent}%")
-        del data
-print("Done")
+from tqdm import tqdm
+import os
+import psutil
+import os
+import argparse
 
-words
-pickle.dump(words,open("lexical_words.pkl","wb"))
-pickle.dump(word_list,open("lexical_word_list.pkl","wb")) # I guess an ordered list of words is also useful
+def collect_words(data_path, collumn_name):
+    #sorting the files for better debugging
+   
+
+
+    sorted_sentences = sorted(os.listdir(data_path))
+
+    for files in sorted_sentences:
+        logging.info(f"Processing {files}")
+    words = Counter()
+    i = 0
+    word_list = []
+    for files in tqdm(sorted_sentences):
+            data = None
+            if i % 10 == 0:
+                pickle.dump(words,open(f"words_{i}.pkl","wb"))
+            if files.endswith(".pkl"):
+                data = pd.read_pickle(os.path.join(data_path,files))
+                if isinstance(data, pd.DataFrame):
+                    print(data.columns)
+                    for word in list(data[collumn_name]):
+                        words[word] += 1
+                        word_list.append(word)
+                else:
+                    print(f"Data is not a DataFrame: {files}")
+            []
+            i += 1
+            print(f"Processed {i} files")
+            print(files)
+            print(f"Memory usage: {psutil.virtual_memory().percent}%")
+            del data
+    print("Done")
+    pickle.dump(words,open("words.pkl","wb"))
+    pickle.dump(word_list,open("word_list.pkl","wb")) # I guess an ordered list of words is also useful
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Collect words from a folder of pickled dataframes")
+    parser.add_argument("--data_path", help="Path to the folder containing the pickled dataframes", default = "../../../../mnt/Restricted/Corpora/CommonVoiceVTL/corpus_as_df_mp_folder_de")
+    parser.add_argument("--collumn_name", help="The name of the collumn containing the words", default = "label")
+    args = parser.parse_args()
+    collect_words(args.data_path, args.collumn_name)
